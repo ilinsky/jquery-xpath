71e5191b3b1df333ec0a882283f57171689c50a6
ilinsky <sergey@ilinsky.com>
2012-12-12 17:05:02 +0200

    Updates document

--- res/build/COPYING.js
+++ res/build/COPYING.js
@@ -1,8 +1,12 @@
 /*
- * XPath2.js - XPath 2.0 implementation in JavaScript
- *
- * Copyright (c) 2012 Sergey Ilinsky
+ * @project.name@ @project.version@
+ * https://github.com/ilinsky/jquery-xpath2.js
+ * Copyright 2012, Sergey Ilinsky
  * Dual licensed under the MIT and GPL licenses.
  *
+ * Includes xpath2.js - XPath 2.0 implementation in JavaScript
+ * https://github.com/ilinsky/xpath2.js
+ * Copyright 2012, Sergey Ilinsky
+ * Dual licensed under the MIT and GPL licenses.
  *
  */
